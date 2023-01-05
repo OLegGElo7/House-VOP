33401329ec9165cf9ad3fb855652ee2f1760f372
Oleg Sinitsin <wg123@yandex.ru>
2023-01-06 03:39:29 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// Scroll to anchors
+(function () {
+  const smoothScroll = function (targetEl, duration) {
+    const headerElHeight = document.querySelector(".header").clientHeight;
+    let target = document.querySelector(targetEl);
+    let targetPosition = target.getBoundingClientRect().top - headerElHeight;
+    let startPosition = window.pageYOffset;
+    let startTime = null;
+
+    const ease = function (t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return (c / 2) * t * t + b;
+      t--;
+      return (-c / 2) * (t * (t - 2) - 1) + b;
+    };
+
+    const animation = function (currentTime) {
+      if (startTime === null) startTime = currentTime;
+      const timeElapsed = currentTime - startTime;
+      const run = ease(timeElapsed, startPosition, targetPosition, duration);
+      window.scrollTo(0, run);
+      if (timeElapsed < duration) requestAnimationFrame(animation);
+    };
+    requestAnimationFrame(animation);
+  };
+
+  const scrollTo = function () {
+    const links = document.querySelectorAll(".js-scroll");
+    links.forEach((each) => {
+      each.addEventListener("click", function () {
+        const currentTarget = this.getAttribute("href");
+        smoothScroll(currentTarget, 1000);
+      });
+    });
+  };
+  scrollTo();
+})();
+
+/* ================= MOBILE NAV ========================= */
+const mobileNavButton = document.querySelector(".header__button");
+const mobileNavIcon = document.querySelector(".header-mobile__button-icon");
+const mobileNav = document.querySelector(".mobile__nav");
+
+mobileNavButton.addEventListener("click", function () {
+  mobileNavIcon.classList.toggle("active");
+  mobileNav.classList.toggle("active");
+  document.body.classList.toggle("no-scroll");
+});
